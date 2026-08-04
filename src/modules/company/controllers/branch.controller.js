549893e218c8434798a395acaa37e63b0bdaaf19
Yogesh Gadhewal <yogeshgadhewal28@gmail.com>
2026-08-04 11:51:33 +0530

    fix: Impersonation (Access POS) issue

--- src/modules/company/controllers/branch.controller.js
+++ src/modules/company/controllers/branch.controller.js
@@ -123,7 +123,7 @@ exports.verifyImpersonationToken = async (req, res) => {
     res.cookie("rms_branch_token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
